2af6222d39bc20a9b6c307c0a92796b102caa6df
YAOSTEPHANE <kouassistephane489@gmail.com>
2023-12-22 11:35:20 +0000

    frontend projet batirem

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,152 +1,86 @@
-import { useEffect, useState } from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import styles from "./Header.module.scss";
-import { FaShoppingCart, FaTimes, FaUserCircle } from "react-icons/fa";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import ShowOnLogin, { ShowOnLogout } from "../hiddenLink/hiddenLink";
-import { useDispatch, useSelector } from "react-redux";
-import { RESET_AUTH, logout } from "../../redux/features/auth/authSlice";
-import { AdminOnlyLink } from "../adminOnlyRoute/AdminOnlyRoute";
-import { UserName } from "../../pages/profile/Profile";
-import {
-  CALCULATE_TOTAL_QUANTITY,
-  selectCartTotalQuantity,
-} from "../../redux/features/product/cartSlice";
+import { Link, NavLink } from 'react-router-dom';
+import { FaShoppingCart, FaTimes } from 'react-icons/fa';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+
+
 
 export const logo = (
-  <div className={styles.logo}>
-    <Link to="/">
-      <h2>
-        Shop<span>Ito</span>.
-      </h2>
-    </Link>
-  </div>
+    <div className={styles.logo}>
+        <Link to='/'>
+            <h2>
+                Batir<span>Em</span>
+            </h2>
+        </Link>
+    </div>
 );
 
-const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
+
+const activeLink = (isActive) => (isActive ? `${styles.active}` : "")
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const [scrollPage, setScrollPage] = useState(false);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    };
+    const hideMenu = () => {
+        setShowMenu(false)
+    };
 
-  const cartTotalQuantity = useSelector(selectCartTotalQuantity);
+    const cart = (
+        <span className={styles.cart}>
+            <Link to="/cart">
+                Panier
+                <FaShoppingCart size={20} />
+                <p>0</p>
+            </Link>
+        </span>
+    );
+    return <header>
+        <div className={styles.header}>{logo}
+            <nav className={showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
 
-  useEffect(() => {
-    dispatch(CALCULATE_TOTAL_QUANTITY());
-  }, [dispatch]);
+                <div className={showMenu ? `${styles["nav-wrapper"]} $
+                {styles["show-nav-wrapper"]}` : `${styles["nav-wrapper"]}
+                `} onClick={hideMenu}>
 
-  const fixNavbar = () => {
-    if (window.scrollY > 50) {
-      setScrollPage(true);
-    } else {
-      setScrollPage(false);
-    }
-  };
-  window.addEventListener("scroll", fixNavbar);
+                </div>
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+                <ul>
+                    <li className={styles["logo-mobile"]}>
+                        {logo}
+                        <FaTimes size={22} color="#fff" onClick={hideMenu} />
 
-  const hideMenu = () => {
-    setShowMenu(false);
-  };
-
-  const logoutUser = async () => {
-    dispatch(RESET_AUTH());
-    await dispatch(logout());
-    localStorage.setItem("cartItems", JSON.stringify([]));
-    navigate("/login");
-    window.location.reload();
-  };
-
-  const cart = (
-    <span className={styles.cart}>
-      <Link to="/cart">
-        Cart
-        <FaShoppingCart size={20} />
-        <p>{cartTotalQuantity}</p>
-      </Link>
-    </span>
-  );
-
-  return (
-    <header className={scrollPage ? `${styles.fixed}` : null}>
-      <div className={styles.header}>
-        {logo}
-
-        <nav
-          className={
-            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
-          }
-        >
-          <div
-            className={
-              showMenu
-                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
-                : `${styles["nav-wrapper"]}`
-            }
-            onClick={hideMenu}
-          ></div>
-
-          <ul onClick={hideMenu}>
-            <li className={styles["logo-mobile"]}>
-              {logo}
-              <FaTimes size={22} color="#fff" onClick={hideMenu} />
-            </li>
-            <li>
-              <NavLink to="/shop" className={activeLink}>
-                Shop
-              </NavLink>
-            </li>
-            <li>
-              <AdminOnlyLink>
-                <NavLink to="/admin/home" className={activeLink}>
-                  | &nbsp; Admin
-                </NavLink>
-              </AdminOnlyLink>
-            </li>
-          </ul>
-          <div className={styles["header-right"]} onClick={hideMenu}>
-            <span className={styles.links}>
-              <ShowOnLogin>
-                <Link to="/profile">
-                  <FaUserCircle size={16} color="#ff7722" />
-                  <UserName />
-                </Link>
-              </ShowOnLogin>
-              <ShowOnLogout>
-                <NavLink to="/login" className={activeLink}>
-                  Login
-                </NavLink>
-                <NavLink to="/register" className={activeLink}>
-                  Register
-                </NavLink>
-              </ShowOnLogout>
-              <ShowOnLogin>
-                <NavLink to="/order-history" className={activeLink}>
-                  My Orders
-                </NavLink>
-              </ShowOnLogin>
-              <ShowOnLogin>
-                <NavLink to="/" onClick={logoutUser}>
-                  Logout
-                </NavLink>
-              </ShowOnLogin>
-            </span>
-            {cart}
-          </div>
-        </nav>
-
-        <div className={styles["menu-icon"]}>
-          {cart}
-          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+                    </li>
+                    <li>
+                        <NavLink to="/shop" className={activeLink} >
+                            Achat
+                        </NavLink>
+                    </li>
+                </ul>
+                <div className={styles["header-right"]}>
+                    <span className={styles.links}>
+                        <NavLink to={"login"} className={activeLink}>
+                            Se connecter
+                        </NavLink>
+                        <NavLink to={"register"} className={activeLink}>
+                            Creer un compte
+                        </NavLink>
+                        <NavLink to={"order-history"} className={activeLink}>
+                            Ma Commande
+                        </NavLink>
+                    </span>
+                    {cart}
+                </div>
+            </nav>
+            <div className={styles["menu-icon"]}>
+                {cart}
+                <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+            </div>
         </div>
-      </div>
-    </header>
-  );
+    </header>;
+
 };
 
 export default Header;
